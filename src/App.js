80174baf99517f4ac0f672bfe80f9dc389dd14ca
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-12-21 18:59:44 +0100

    add data from data.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@
 import React from 'react'
 import './App.css';
 import Jokes from './Jokes'
+import data from './data'
 
 function App() {
+
+
   return (
     <div className="App">
-
+      {allJokes}
     </div>
   );
 }
