42053e667e994c2e7a981b3f045a11844f998ae0
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-12-21 19:56:30 +0100

    update conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 
 import React from 'react'
 import './App.css';
-import Jokes from './Jokes'
-import data from './data'
+// import Jokes from './Jokes'
+// import data from './data'
 
 function App() {
+  
+  const [messages, setMessages] = React.useState(["a", "b", "c"])
 
-const [joke, setJoke] = React.useState(data)
+// const [joke, setJoke] = React.useState(data)
 
-const allJokes = joke.map(singleJoke => <Jokes 
-title={singleJoke.setup} 
-punchline={singleJoke.punchline}
-key={singleJoke.id}
-/>)
+// const allJokes = joke.map(singleJoke => <Jokes 
+// title={singleJoke.setup} 
+// punchline={singleJoke.punchline}
+// key={singleJoke.id}
+// />)
 
   return (
     <div className="App">
-      {allJokes}
+      {/* {allJokes} */}
+      {messages.length > 0 && <h1>You have {messages.length} unread messages!</h1>}
     </div>
   );
 }
